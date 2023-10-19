3d7133fb8eec39fce46d22dbf26b231ce0d3a887
rae <633012+okdistribute@users.noreply.github.com>
2023-10-18 20:10:45 -0700

    Update ditto.ts

--- react/src/ditto.ts
+++ react/src/ditto.ts
@@ -1,15 +1,15 @@
-import { Ditto, IdentityOfflinePlayground } from '@dittolive/ditto'
+import { Ditto, IdentityOnlinePlayground } from '@dittolive/ditto'
 
 let ditto: Ditto
 export default function get() {
   if (!ditto) {
-    const identity: IdentityOfflinePlayground = {
-      type: 'offlinePlayground',
-      appID: "dql-demo"
+    const identity: IdentityOnlinePlayground = {
+      type: 'onlinePlayground',
+      appID: "",
+      token: ""
     }
     ditto = new Ditto(identity, '/ditto')
     ditto.startSync()
-    ditto.setOfflineOnlyLicenseToken("o2d1c2VyX2lka0NoaWNrLWZpbC1BZmV4cGlyeXgYMjAyMy0wNC0wOVQwNjo1OTo1OS45OTlaaXNpZ25hdHVyZXhYUmg1TUVSZE12TWxhYWZkNkp4T2VPZFE5cXBleXh2a1FLQlhXZlIrcXQvRXBtOUFqOUwvRGRxOFEvcmdEelB4anRndXJwL2lyandsaDlic3A3T1JMZVE9PQ==")
   }
   return ditto
 }
